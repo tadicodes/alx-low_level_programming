6d31bbcd6b610521cc113f15e2494e7424ab562c
tadicodes <tadishongedza@gmail.com>
2023-02-17 18:51:51 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print all single digits of base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	putchar('\n');
+
+	return (0);
+}
