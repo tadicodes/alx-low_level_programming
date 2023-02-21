489a08379b6aa923b4607e0323a6c34640c3a575
tadicodes <tadishongedza@gmail.com>
2023-02-21 05:30:11 -0800

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - a program that prints _putchar
+ * is written by mbah
  * Return: 0
  */
+
 int main(void)
 {
-	putchar("\_putchar");
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
