6d7808f3a72ce81c5d56b3e7b6fc58ab48defa34
tadicodes <tadishongedza@gmail.com>
2023-02-27 11:04:26 -0800

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - checks the code
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char s[10] = "My School";
+
+	printf("%s\n", s);
+	rev_string(s);
+	printf("%s\n", s);
+	return (0);
+}
