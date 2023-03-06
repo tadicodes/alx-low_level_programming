b9f3c0ce0a024c1ca258f78ce25fd2c136dfa423
tadicodes <tadishongedza@gmail.com>
2023-03-06 03:42:18 -0800

    Update

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,12 @@
-
+#include "main.h"
+/**
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
 		int k;
 
 		while (*s)
