36f54ca58ef59da740d072a9d2af571fc9f85426
tadicodes <tadishongedza@gmail.com>
2023-03-30 06:48:12 -0700

    Update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,15 @@
+#include "lists.h"
 #include <stdio.h>
 
-void first(void) __attribute__ ((constructor));
+void print_fast(void) __attribute__ ((constructor));
 
 /**
- * first - prints a sentence before the main
- * function is executed
- */
-void first(void)
+  * print_fast - prints before main
+  *
+  * Return: void
+  */
+void print_fast(void)
 {
- printf("You're beat! and yet, you must allow,\n");
- printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
