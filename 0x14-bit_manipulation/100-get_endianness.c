6f1a385b17f6c7e74438bfe1941751da63228d64
tadicodes <tadishongedza@gmail.com>
2023-05-06 15:59:21 -0700

    Advanced task 1

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks for small or big endian
+ * Return: 0 for big and 1 for small
+ */
+
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *) &i;
+
+	return (*c);
+}
