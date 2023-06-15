821f0f36cf9b6c08a1eae7c6ce10ab8010ccf9f5
tadicodes <tadishongedza@gmail.com>
2023-06-15 03:42:08 -0700

    commit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements in a dlistint_t list
+ * printis_dlistint - prints all the elements in a dlistint_t list
  * @h: pointer to list
  * Return: number of nodes
  **/
-size_t prints_dlistint(const dlistint_t *h)
+size_t printis_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *node = h;
 	size_t cont = 0;
